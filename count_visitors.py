b34dc794556ec28ce8236037a482e9ce6e7034dd
zhanglei0103 <zxy1419@mail.ustc.edu.cn>
2025-08-10 13:32:08 +0800

    Update count_visitors.py

--- count_visitors.py
+++ count_visitors.py
@@ -1,51 +1,12 @@
 # -*- coding: utf-8 -*-
-import os
-import sys
-import pandas as pd
+
 import streamlit as st
-import numpy as np
-import pathlib
-from bs4 import BeautifulSoup
-import logging
-import shutil
-
-def inject_ga():
-    GA_ID = "google_analytics"
-
-
-    GA_JS = """
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-**********"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-**********');
-    </script>
-    """
-
-    # Insert the script in the head tag of the static template inside your virtual
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    # st.write(f'editing {index_path}')
-    logging.info(f'editing {index_path}')
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID):
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)
-        else:
-            shutil.copy(index_path, bck_index)
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_JS)
-        # st.write(new_html)
-        index_path.write_text(new_html)
-
-
-inject_ga()
-
-st.write('hello world!')
-
+import pandas as pd
+ 
+st.write("""
+# My first app
+Hello *world!*
+""")
 
 
 
